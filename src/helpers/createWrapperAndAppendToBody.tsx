3ef2399fa6273f842d639a70ff2f031cc24cb13a
Gor Sargsyan <gorsargsyanm@gmail.com>
2023-12-15 05:36:15 +0400

    fix quotes by prettier

--- src/helpers/createWrapperAndAppendToBody.tsx
+++ src/helpers/createWrapperAndAppendToBody.tsx
@@ -1,6 +1,6 @@
 function createWrapperAndAppendToBody(wrapperId: string) {
-  const wrapperElement = document.createElement("div");
-  wrapperElement.setAttribute("id", wrapperId);
+  const wrapperElement = document.createElement('div');
+  wrapperElement.setAttribute('id', wrapperId);
   document.body.appendChild(wrapperElement);
   return wrapperElement;
 }
