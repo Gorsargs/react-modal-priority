996028b85f6125d33fb5ffda8f99632f8ddf34e0
Gor Sargsyan <gorsargsyanm@gmail.com>
2024-02-02 17:34:28 +0400

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
