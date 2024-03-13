17b7df1c5b2316a14923c19872cd0cca1e3ee4cc
Gor Sargsyan <gorsargsyanm@gmail.com>
2024-03-13 07:41:08 +0400

    fix: export IModalManager type

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { ModalProvider, usePriorityModal } from './src/context/ModalContext';
 export { useCurrentModal } from './src/context/CurrentModalContext';
 export type { ModalParams } from './src/core/Modal';
+export type { IModalManager } from './src/core/ModalManager';
